3b51b1f1abcae5f0532dbf18f2f76b62d1ac1c71
CastZel1x <casterzel1x@gmail.com>
2022-10-21 20:46:14 +0300

    big update

--- src/HUD/Players/Player.tsx
+++ src/HUD/Players/Player.tsx
@@ -55,45 +55,21 @@ export default class PlayerBox extends React.Component<IProps, IState> {
       <div className={`player ${player.state.health === 0 ? "dead" : ""} ${this.props.isObserved ? 'active' : ''}`}>
         <div className="player_data">
          <Avatar steamid={player.steamid} height={57} width={57} showSkull={false} showCam={false} sidePlayer={true} />
+         <div className="obs_ps">  <div className="num">{player.observer_slot}</div>   </div>
           <div className="player_stats">
+          { player.state.health > 0 ? <div className={`hp_bar hp_bar_bg`} style={{ width: `${player.state.health}%` }}></div> : null}
+            <div className={`hp_bar ${player.state.health <= 20 ? 'low':''}`} style={{ width: `${player.state.health}%` }}></div>
+
+
+
             <div className="row">
-            <div className="obs_ps">
-              <div className="num">{player.observer_slot}</div>
-            </div>
               <div className="health">
               {player.state.health > 0 ? player.state.health :""}
               </div>
               <div className="username">
                 <div>{player.name}</div>
               </div>
-              <div className="weapon">
-              {primary || secondary ? <Weapon weapon={primary ? primary.name : secondary.name} active={primary ? primary.state === "active" : secondary.state === "active"} /> : ""}
-              </div>
-            </div>
-            { player.state.health > 0 ? <div className={`hp_bar hp_bar_bg`} style={{ width: `${player.state.health}%` }}></div> : null}
-            <div className={`hp_bar ${player.state.health <= 20 ? 'low':''}`} style={{ width: `${player.state.health}%` }}></div>
-            <div className="row">
-            <div className="armor_and_utility">
-                <Bomb player={player} />
-                <Armor player={player} />
-                <Defuse player={player} />
-              </div>
-              <div className="dead-adr">
-                    <div className="labels">
-                   <div className="stat-label">ADR</div>
-                      </div>
-                    <div className="values">
-                    <div className="stat-value">{player.state.adr}</div>
-                </div>
-               </div>
-              <div className="money">${player.state.money}</div>
-              <div className={`spending ${this.props.isFreezetime && this.props.isFreezetime === true ? 'show' : 'hide'}`}>
-                  <div className="value">-${moneySpent}</div>
-              </div>
-              <div className={`adr ${this.props.isFreezetime && this.props.isFreezetime === true ? 'show' : 'hide'}`}>
-                  <div className="stat-label">ADR</div>
-                  <div className="stat-value">{player.state.adr}</div>
-              </div>
+              {player.state.round_kills ? <div className="roundkills-container">{player.state.round_kills}</div> : null}
               <div className="grenades">
                 {grenades.map(grenade => (
                   [
@@ -102,26 +78,42 @@ export default class PlayerBox extends React.Component<IProps, IState> {
                   ]
                 ))}
               </div>
+            </div>
 
 
 
-              <div className="statistics">
+            <div className="row_center">
+              <div className="weapon">
+              {primary || secondary ? <Weapon weapon={primary ? primary.name : secondary.name} active={primary ? primary.state === "active" : secondary.state === "active"} /> : ""}
+              </div>
+              <div className="statt">
 
                 <img className="kill" src= {kill}
-                  width="22px" height="19px" alt="filter applied" />  
+                  width="18px" height="18px" alt="filter applied" />  
                 <Statistic label={""} value={stats.kills} />
 
-                <div className="spacekildead"></div>
 
                 <img className="dead" src= {dead} 
-                  width="21px" height="18px" alt="filter applied" />  
+                  width="16px" height="17px" alt="filter applied" />  
                 <Statistic label={""} value={stats.deaths} />
               </div>
-
-
-              {player.state.round_kills ? <div className="roundkills-container">{player.state.round_kills}</div> : null}
-              <div className={`secondary_weapon ${this.props.isFreezetime && this.props.isFreezetime === true ? 'hide' : 'show'}`}>{primary && secondary ? <Weapon weapon={secondary.name} active={secondary.state === "active"} /> : ""}</div>
             </div>
+
+
+
+
+            <div className="row_top">
+              <div className="money">${player.state.money}</div>
+              <div className={`spending ${this.props.isFreezetime && this.props.isFreezetime === true ? 'show' : 'hide'}`}>
+                  <div className="value">-${moneySpent}</div>
+              </div>
+              <div className={`adr ${this.props.isFreezetime && this.props.isFreezetime === true ? 'show' : 'hide'}`}>
+                  <div className="stat-label">ADR</div>
+                  <div className="stat-value">{player.state.adr}</div>
+              </div>
+            </div>
+
+
             <div className="active_border"></div>
           </div>
         </div>
